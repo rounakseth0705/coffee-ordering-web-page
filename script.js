05baf6c7a19ca3bad855f4b71e5f51332e371911
Rounak Seth <sethrounak7@gmail.com>
2025-07-28 04:28:02 +0530

    Add footer section

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -6,7 +7,13 @@ menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 })
 
+// Close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Close menu when the nav link is clicked
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click())
+})
 
 // Initialize Swiper
 const swiper = new Swiper('.slider-wrapper', {
